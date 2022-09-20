777c843f4a7aeeb3954ca692242533ad6f829d77
caoming <cm9999@qq.com>
2022-09-20 23:06:53 +0800

    init tests/slam

---
+++ tests/pose_graph_3d.cc
@@ -0,0 +1,11 @@
+#include "unos/unos.hh"
+#include "tests/slam/types.hh"
+#include "gtest/gtest.h"
+
+// Author: vitus@google.com (Michael Vitus)
+// Edit: EpsAvlc
+
+TEST(UNOS_FULL, pose_graph_3d) {
+  unos::tests::MapOfPoses poses;
+  
+}
