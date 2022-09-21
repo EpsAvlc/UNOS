58a0ef01ad44469a8cdeb465032f38f151f0fa84
mars.cao <mars.cao@momenta.ai>
2022-09-21 19:36:25 +0800

    add read_g2o.hh

--- tests/pose_graph_3d.cc
+++ tests/pose_graph_3d.cc
@@ -1,11 +1,18 @@
+#include <filesystem>
+
 #include "unos/unos.hh"
 #include "tests/slam/types.hh"
+#include "tests/slam/read_g2o.hh"
 #include "gtest/gtest.h"
 
 // Author: vitus@google.com (Michael Vitus)
 // Edit: EpsAvlc
 
-TEST(UNOS_FULL, pose_graph_3d) {
+TEST(UNOS, pose_graph_3d) {
   unos::tests::MapOfPoses poses;
-  
+  unos::tests::VectorOfConstraints constraints;
+  char current_path[PATH_MAX];
+  getcwd(current_path, sizeof(current_path));
+  std::string g2o_file = current_path + "../";
+  // unos::tests::readG2oFile()
 }
